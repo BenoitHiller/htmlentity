83cd2a6d0bb91f95cbd42d8d8b655dd8c2d9a851
Benoit Hiller <benoit.hiller@gmail.com>
2016-09-17 10:18:08 -0400

    adding missing entities
    closes #1

--- test/decodetest.bash
+++ test/decodetest.bash
@@ -9,5 +9,10 @@ testDecode() {
   diff <( awk -F "\r" '{ printf("%s\n", $2) }' htmlentity.lst ) <(decodehtml.bash < <( awk -F "\r" '{ printf("%s\n", $1) }' htmlentity.lst ) ) || fail
 }
 
+testCombining() {
+  diff <( echo -e "A\xe2\x83\x9c") <(decodehtml.bash <<< "A&DotDot;") || fail
+}
+
 testDecode
+testCombining
 echo "test passed"
